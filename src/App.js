5a4d6ee5e0bf1dccff070999cc59706f3b7e6b13
Bouyahi Mohamed <bouyahi.mohamed.1@gmail.com>
2025-09-28 22:57:55 +0200

    Add routing and page structure for e-commerce application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import React from 'react';
+import {Routes, Route,Link} from 'react-router-dom';
+import Home from './pages';
+import ProductDetails from './pages/ProductDetails';
+import Order from './pages/order';
+import Checkout from './pages/checkout';
 
 function App() {
   return (
-    <div>hi again </div>
+    <div className="App">
+      <nav>
+        <h1>E-commerce Site</h1>
+        <Link to="/">Home</Link> | <Link to="/order">Order</Link> | <Link to="/checkout">Checkout</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/product/:id' element={<ProductDetails/>}/>
+        <Route path='/order' element={<Order/>}/>
+        <Route path='/checkout' element={<Checkout/>}/>
+        <Route path='*' element={<div>404 Not Found</div>}/>
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,6 @@
+
+
+function ProductDetails() {
+  return <div>Product Details Page</div>;
+}
+export default ProductDetails;
